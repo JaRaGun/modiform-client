d16d7b08eb9d580f9f19c9e2a0beac22e9e05a4d
TABRILLA <angelestabrilla10@gmail.com>
2023-12-10 10:52:12 +0800

    i

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
-function App() {
+import MainRoutes from "./routes"
+const App = () => {
   return (
-    <div className="bg-[#10172A] min-h-screen flex flex-col items-center justify-center">
-      <div className="mb-8">
-        <img
-          src="https://miro.medium.com/v2/resize:fit:1400/1*poaGV4iICp06Q-yTlA2g_g.png"
-          className="w-auto h-36 rounded-md"
-        />
-      </div>
-      <h1 className="text-3xl font-bold mb-4 text-white"></h1>
-      <div className="card bg-white p-4 rounded-md shadow-md mt-4">
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </div>
-  );
+    <MainRoutes/>
+  )
 }
 
-export default App;
+export default App
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { Login } from "../pages";
 import { Route, Routes } from "react-router-dom";
-import PrivateRoutes from "./PrivateRoutes";
+// import PrivateRoutes from "./PrivateRoutes";
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route element={<PrivateRoutes />}>
-        {/* Add the Protected Routes here */}
-      </Route>
+      {/* <Route element={<PrivateRoutes />}>
+        
+      </Route> */}
+
       <Route element={<Login />} path="/" />
     </Routes>
   );
