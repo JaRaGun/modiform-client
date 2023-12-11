7f96d999c4121fb5154925cf97fcec12e137f75a
TABRILLA <angelestabrilla10@gmail.com>
2023-12-11 11:19:05 +0800

    HomePage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,72 @@
+import { Button } from "@mui/material";
 import Navbar from "../../components/navbar/Navbar";
 import images from "../../themes/images";
 
 const Home = () => {
-  return <div>
-      <Navbar/>
-        <div className=""
-            style={{
-              backgroundImage: `url(${images.HOMEBG})`,
-              backgroundSize: 'cover',
-              
-              minHeight: 'calc(100dvh + 130px)',
-              backgroundPosition: "center",
-            }}>
+  return (
+    <div>
+      <Navbar />
+      <div
+        className=""
+        style={{
+          backgroundImage: `url(${images.HOMEBG})`,
+          backgroundSize: "cover",
+          minHeight: "calc(100dvh + 130px)",
+          backgroundPosition: "center",
+        }}
+      ></div>
+
+      <div
+        style={{
+          backgroundImage: `url(${images.BLURRYBG})`,
+          backgroundSize: "cover",
+          minHeight: "calc(100vh + 130px)",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="flex justify-center items-center py-20 sm:py-20 md:py-24 lg:py-24">
+          <Button variant="contained">RESERVE NOW</Button>
         </div>
 
-        <div className=""
-            style={{
-              backgroundImage: `url(${images.BACKGROUNDSTI})`,
-              backgroundSize: 'cover',
-              
-              minHeight: 'calc(100dvh + 130px)',
-              backgroundPosition: "center",
-            }}>
+        <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-0">
+          <div className="relative group">
+            <img
+              src={images.COLLEGE}
+              alt="COLLEGEPIC"
+              className="transition duration-300 ease-in-out"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 text-white text-3xl text-center opacity-0 transition-opacity duration-300 ease-in-out group-hover:bg-opacity-50 group-hover:opacity-100">
+              College
+            </div>
+          </div>
 
-
+          <div className="relative group">
+            <img
+              src={images.SENIORHIGH}
+              alt="SHSPIC"
+              className="transition duration-300 ease-in-out"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 text-white text-3xl text-center opacity-0 transition-opacity duration-300 ease-in-out group-hover:bg-opacity-50 group-hover:opacity-100">
+              Senior High
+            </div>
+          </div>
         </div>
 
-  </div>;
+        <div>
+          <div className="relative group">
+            <img
+              src={images.OTHERS}
+              alt="COLLEGEPIC"
+              className="transition duration-300 ease-in-out"
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 text-white text-3xl text-center opacity-0 transition-opacity duration-300 ease-in-out group-hover:bg-opacity-50 group-hover:opacity-100">
+              OTHERS
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/themes/images.ts
+++ src/themes/images.ts
@@ -3,5 +3,9 @@ import MODILOGO from '../assets/IMGS/Modiform.png'
 import BACKGROUNDSTI from '../assets/IMGS/BackgroundSTI.png'
 import WHITEMODIFORM from '../assets/IMGS/WhiteModiform.png'
 import HOMEBG from '../assets/IMGS/HomeBG.png'
+import BLURRYBG from '../assets/IMGS/BlurryBG.png'
+import COLLEGE from '../assets/IMGS/College.png'
+import SENIORHIGH from '../assets/IMGS/SeniorHigh.png'
+import OTHERS from '../assets/IMGS/Others.png'
 
-export default { MODILOGO, BACKGROUNDSTI, WHITEMODIFORM, HOMEBG }
+export default { MODILOGO, BACKGROUNDSTI, WHITEMODIFORM, HOMEBG, BLURRYBG, COLLEGE, SENIORHIGH, OTHERS }
