f57c2056c97c13cd1ee7587a591acc0145b54aea
Kenneth Aduan <kenneth.aduan10@gmail.com>
2023-12-19 07:47:36 +0800

    done

--- src/firebase/services/index.ts
+++ src/firebase/services/index.ts
@@ -10,9 +10,20 @@ import {
   //   setDoc,
   //   updateDoc,
   deleteDoc,
+  Timestamp,
   //   getDoc,
 } from "firebase/firestore";
+const generateRandomNumber = (): number => {
+  return Math.floor(10000 + Math.random() * 90000);
+};
 
+const getCurrentDate = (): string => {
+  const date = new Date();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2); // Months are 0 based, so add 1 and format as 2 digits
+  const day = ("0" + date.getDate()).slice(-2); // Format as 2 digits
+  const year = date.getFullYear().toString().slice(-2); // Get last 2 digits of year
+  return `${month}/${day}/${year}`;
+};
 export const checkUserData = async (
   studentId: number | null,
   password: string
@@ -81,6 +92,106 @@ export const removeToCartFirebase = async (
     }
   } catch (error) {
     console.error("Error removing item:", error);
+    throw error;
+  }
+};
+
+export const AddItemOrderToFirebase = async (
+  studentId: number | null,
+  cartItems: any[],
+  username: string,
+  totalItems: number,
+  totalAmount: number
+) => {
+  //  Add Item order
+  // console.log("studentId", studentId);
+
+  // console.log("cartItems", cartItems);
+  const userRef = collection(db, "orders");
+  await addDoc(userRef, {
+    studentId,
+    orderDate: getCurrentDate(),
+    orderNumber: generateRandomNumber(),
+    totalItems,
+    totalAmount,
+    username,
+  });
+  try {
+    const querySnapshot = await getDocs(
+      query(userRef, where("studentId", "==", studentId))
+    );
+
+    if (!querySnapshot.empty) {
+      const orderDoc = querySnapshot.docs[0];
+      const invoiceRef = collection(db, "orders", orderDoc.id, "odersItems");
+
+      // Add each cart item as a separate document in the invoice collection
+      for (const item of cartItems) {
+        await addDoc(invoiceRef, item);
+      }
+    }
+    return querySnapshot;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+};
+
+export const RemoveInnerCollectionFirebase = async (
+  parentCollectionName: string,
+  innerCollectionName: string,
+  studentId: number | null
+) => {
+  try {
+    const userRef = collection(db, parentCollectionName);
+    const querySnapshot = await getDocs(
+      query(userRef, where("studentId", "==", studentId))
+    );
+    if (!querySnapshot.empty) {
+      const userDoc = querySnapshot.docs[0];
+      const cartRef = collection(
+        db,
+        parentCollectionName,
+        userDoc.id,
+        innerCollectionName
+      );
+      const cartSnapshot = await getDocs(cartRef);
+      if (!cartSnapshot.empty) {
+        cartSnapshot.forEach((docSnapshot) => {
+          deleteDoc(
+            doc(
+              db,
+              parentCollectionName,
+              userDoc.id,
+              innerCollectionName,
+              docSnapshot.id
+            )
+          );
+        });
+      }
+    }
+    return querySnapshot;
+  } catch (error) {
+    console.error("Error removing item:", error);
+    throw error;
+  }
+};
+
+export const AddNotificationsToFirebase = async (
+  studentId: number | null,
+  message: string
+) => {
+  try {
+    const userRef = collection(db, "notifications");
+    await addDoc(userRef, {
+      id: generateRandomNumber(),
+      title: "Client User",
+      senderId: studentId,
+      message: message,
+      date: Timestamp.fromDate(new Date()), // Use Firestore Timestamp
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
     throw error;
   }
 };
--- src/pages/Home/cart/Checkout/CheckOutButton.tsx
+++ src/pages/Home/cart/Checkout/CheckOutButton.tsx
@@ -1,13 +1,59 @@
-import { useAppSelector } from "../../../../utils/redux/hooks";
+import { useAppSelector, useAppDispatch } from "../../../../utils/redux/hooks";
+import {
+  AddItemOrderToFirebase,
+  RemoveInnerCollectionFirebase,
+  AddNotificationsToFirebase,
+} from "../../../../firebase/services";
+import Swal from "sweetalert2";
+import { clearCartRedux } from "../../../../utils/redux/slice/cartSlice";
 
 const CheckOutButton = () => {
   const cartItems = useAppSelector((state) => state.cart.items);
-
+  const { studentIdRedux, firstNameRedux } = useAppSelector(
+    (state) => state.user
+  );
+  const dispatch = useAppDispatch();
   // Calculate the total price
   const totalPrice = cartItems.reduce(
     (total, item) => total + item.totalPrice,
     0
   );
+  const totalQuantity = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
+  const message: string = "New Item Reservation";
+  const handleAddOrder = async () => {
+    Swal.fire({
+      title: "Reservation Confirmation",
+      text: "Are you sure you want to reserved this item(s)?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Checkout",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        await AddItemOrderToFirebase(
+          studentIdRedux,
+          cartItems,
+          firstNameRedux,
+          totalQuantity,
+          totalPrice
+        );
+
+        await RemoveInnerCollectionFirebase("users", "cart", studentIdRedux);
+        await dispatch(clearCartRedux());
+        await AddNotificationsToFirebase(studentIdRedux, message);
+        Swal.fire({
+          title: "Success!",
+          text: "Please wait... for the item to be reserved",
+          icon: "success",
+        });
+      }
+    });
+  };
   // console.log(cartItems);
   return (
     <div className="w-full px-5 py-5p">
@@ -28,8 +74,11 @@ const CheckOutButton = () => {
           <span className="font-semibold">ORDER TOTAL</span>
           <span className="text-xl font-bold">₱ {totalPrice.toFixed(2)}</span>
         </div>
-        <button className="w-full px-4 py-2 mt-6 text-white bg-blue-500 rounded-lg">
-          Checkout
+        <button
+          className="w-full px-4 py-2 mt-6 text-white bg-blue-500 rounded-lg"
+          onClick={handleAddOrder}
+        >
+          RESERVE
         </button>
       </div>
     </div>
